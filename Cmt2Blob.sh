9bccf3805e235a7e3ea4a5077140c912a7f36518
Audris Mockus <audris@utk.edu>
2017-11-29 10:40:32 -0500

    stashing changes

--- Cmt2Blob.sh
+++ Cmt2Blob.sh
@@ -6,5 +6,5 @@ mv /data/commit_blob.tch /fast1/All.sha1c/
 
 #use old /fast1/All.sha1c/blob_commit.tch 
 ./exportObj1.perl /fast1/All.sha1c/blob_commit.tch | gzip > /data/All.blobs/blob_commit.bin
-gunzip -c /da0_data/c2fbp/c2fbp.{7[4-9],8[0-9]}.gz | cut -d\; -f1,3 | uniq | perl -I ~/lib64/perl5 Blob2CommitMerge.perl /data/blob_commit.bin /data/blob_commit.tch
+gunzip -c /da0_data/c2fbp/c2fbp.{7[4-9],8[0-9]}.gz | cut -d\; -f1,3 | uniq | perl -I ~/lib64/perl5 Blob2CmtMerge.perl /data/blob_commit.bin /data/blob_commit.tch
 mv /data/blob_commit.tch /fast1/All.sha1c/
