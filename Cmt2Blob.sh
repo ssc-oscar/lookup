1724442a13ea1fecda70b2b8fb55da7fa5fb7d93
Audris Mockus <audris@utk.edu>
2017-11-29 10:38:38 -0500

    finished C2B

--- Cmt2Blob.sh
+++ Cmt2Blob.sh
@@ -1,3 +1,19 @@
+#Do fresh
+
+for i in {000..80}; do for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2Bin.pbs | qsub; done; done
+for i in {000..78..02}; do for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2merge.pbs | qsub; done; done
+for i in {000..76..04}; do for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2merge1.pbs | qsub; done; done
+for i in {000..72..08}; do for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2merge2.pbs | qsub; done; done
+for i in {000..64..16}; do for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2merge3.pbs | qsub; done; done
+for i in {000..32..32}; do for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2merge4.pbs | qsub; done; done
+for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2BmergeS.pbs | qsub; done
+for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2BmergeS1.pbs | qsub; done
+for j in {0..15}; do sed "s/NNN/$i/g;s/MMM/$j/g" doC2BmergeS2.pbs | qsub; done
+#resupt is Cmt2Blob.00-80.{0..15}.tch
+
+
+
+
 #use the result of old Cmt2Blob.perl in commit_blob.tch
 mv /fast1/All.sha1c/commit_blob.tch /fast1/All.sha1c/commit_blob.tch.old
 ./exportObj.perl /fast1/All.sha1c/commit_blob.tch.old | gzip > /data/All.blobs/commit_blob.bin
---
+++ Cmt2BlobBin.perl
@@ -0,0 +1,54 @@
+use strict;
+use warnings;
+use Error qw(:try);
+
+use TokyoCabinet;
+use Compress::LZF;
+
+sub toHex { 
+        return unpack "H*", $_[0]; 
+} 
+
+sub fromHex { 
+        return pack "H*", $_[0]; 
+} 
+
+my %b2c;
+my $sec;
+
+for $sec (0..15){
+  tie %{$b2c{$sec}}, "TokyoCabinet::HDB", "$ARGV[0].$sec.tch", TokyoCabinet::HDB::OWRITER | TokyoCabinet::HDB::OCREAT,   
+        16777213, -1, -1, TokyoCabinet::TDB::TLARGE, 100000
+     or die "cant open $ARGV[0].$sec.tch\n";
+}
+
+my %b2c1;
+my $lines = 0;
+while (<STDIN>){
+  chop();
+  my ($c, $bb) = split(/\;/, $_, -1);
+  my $cmt = fromHex ($c);
+  my $b = fromHex ($bb);
+  $b2c1{$cmt}{$b}++;
+  $lines ++;
+  print STDERR "$lines read\n" if (!($lines%100000000));
+}
+
+output ();
+
+for $sec (0..15){
+  untie %{$b2c{$sec}};
+}
+
+sub output {
+  while (my ($k, $v) = each %b2c1){
+    my @shas = sort keys %{$v};
+    my $v1 = join '', @shas;
+    my $sec = (unpack "C", substr ($k, 0, 1))%16;
+    $b2c{$sec}{$k} = $v1;
+  }
+}
+
+
+
+
