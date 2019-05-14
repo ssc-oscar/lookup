3e88789c4bf6e3246afa6f4895f13d04f6d5c1c5
dkennard3 <dkennard@vols.utk.edu>
2019-05-14 16:41:23 -0400

    added code that grabs authors associated with user, & grab projects an author worked on

--- dpg.py
+++ dpg.py
@@ -1,16 +1,37 @@
 import pymongo
-'''
-from oscar import Author
-'''
+from oscar import *
 
 client = pymongo.MongoClient("mongodb://da1.eecs.utk.edu/")
 
 db = client["WoC"]
-col = db["authors"]
-cursor = col.find({})
+col = db["users"]
+users = col.find({})
 
-for author in cursor:
-	print(author['id'])
+## get author ids for each user
+user2authors = {}
 
-
-
+for user in users:
+	user2authors[user['_id']] = []
+	for selectId in user['selectedIds']:
+		if isinstance(selectId, basestring):
+			user2authors[user['_id']].append(selectId)
+		elif isinstance(selectId, dict):
+			user2authors[user['_id']].append(selectId['id'])
+'''
+for key, val in user2authors.items():
+	print(key),
+	print(":"),
+	print(val)
+'''
+## get projects for a user
+a2p = {}
+for user in user2authors.keys():
+	for author_id in user2authors[user]:
+		projects = Author(author_id.encode('utf-8')).project_names
+		a2p[author_id] = projects
+'''
+for key, val in a2p.items():
+	print(key),
+	print(":"),
+	print(val)
+'''
