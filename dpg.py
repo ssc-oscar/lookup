07b9429c9ad0b4c676330dabd9a9f06f66be9621
dkennard3 <dkennard@vols.utk.edu>
2019-05-18 14:56:12 -0400

    added a2f and p2c dictionaries, and began translating 2nd half of dpg.perl

--- dpg.py
+++ dpg.py
@@ -1,4 +1,5 @@
 import pymongo
+import operator
 from oscar import *
 
 badProjects = {
@@ -40,7 +41,9 @@ for user in input.keys():
 	a2p[user] = []
 	for author_id in input[user]:
 		projects = Author(author_id.encode('utf-8')).project_names
-		a2p[user].append(projects)
+		for proj in projects:
+			if proj not in a2p[user]:
+				a2p[user].append(proj)
 '''
 for key, val in a2p.items():
 	print(key),
@@ -63,19 +66,18 @@ gA = {}		## global authors
 u2a = {}
 for user in input.keys():
 	p2a = {}
-	for project_tuple in a2p[user]:
-		for proj in project_tuple:
-			if proj in badProjects.keys() or proj in p2a.keys():
+	for proj in a2p[user]:
+		if proj in badProjects.keys() or proj in p2a.keys():
+			continue
+		a_names = Project(proj).author_names
+		p2a[proj] = []
+		for author in a_names:
+			if author in str_version[user]:
 				continue
-			a_names = Project(proj).author_names
-			p2a[proj] = []
-			for author in a_names:
-				if author in str_version[user]:
-					continue
-				p2a[proj].append(author)
-				if author not in gA.keys():
-					gA[author] = []
-				gA[author].append(proj)
+			p2a[proj].append(author)
+			if author not in gA.keys():
+				gA[author] = []
+			gA[author].append(proj)
 	u2a[user] = p2a
 
 '''
@@ -118,7 +120,6 @@ for key, val in a2c.items():
 '''
 #####################
 ## get blobs for a user
-
 a2b = {}
 for user in input.keys():
 	a2b[user] = []
@@ -126,7 +127,6 @@ for user in input.keys():
 		if Commit(commit).blob_shas_rel:
 			for blob in Commit(commit).blob_shas_rel:
 				a2b[user].append(blob) 
-
 '''
 for key, val in a2b.items():
 	print(key),
@@ -135,6 +135,95 @@ for key, val in a2b.items():
 '''
 #####################
 ## get files for a user
+
+a2f = {}
+for user in input.keys():
+	a2f[user] = []
+	for commit in a2c[user]:
+		for file in Commit(commit).files:
+			a2f[user].append(file)	
+'''
+for key, val in a2f.items():
+	print(key),
+	print(":"),
+	print(val)
+'''
+#####################
+## get commits for projects
+
+p2c = {}
+p2nc = {}
+for user in input.keys():
+	for proj in a2p[user]:
+		if proj not in p2c.keys() or proj not in p2nc.keys():
+			p2c[proj] = []
+			p2nc[proj] = 0
+		for commit in Project(proj).commit_shas:
+			p2c[proj].append(commit)
+			p2nc[proj] += 1
+'''
+for key, val in p2nc.items():
+	print(key),
+	print(":"),
+	print(val)
+'''
+'''
+prof = db["profile"].find({})
+PS = db["projects"].find({})
+BS = db["blobs"].find({})
+AS = db["friends"].find({})
+
+for user in input.keys():
+	result = {}
+	A = []
+	Af = []
+	P = []
+	Pf = []
+	B = []
+	Bf = []
+	
+	authors = u2a[user]
+	commits = a2c[user]
+	blobs = a2b[user]
+	files = a2f[user]
+	projects = a2p[user]
+	clb = {}
+	clb1 = {}
+
+	for proj in projects:
+		for author in sorted(authors.items(), key = lambda x: p2nc.get(x[0])):
+			if author not in clb.keys():
+				clb[author] = []
+			clb.append(proj)
+			if proj not in clb1.keys():
+				clb1[proj] = []
+			clb1.append(author)
+		nMyC = 0
+		for commit in commits:
+			if commit in p2c[proj]:
+				nMyC += 1
+		url = Project(proj).toURL()
+
+		if len(P) < 30:
+			temp = {}
+			temp['name'] = proj
+			temp['nC'] = p2nc[proj]
+			temp['nMyC'] = nMyC
+			temp['url'] = url
+			P.append(temp)
+		temp = {}
+		temp['name'] = proj
+		temp['nC'] = p2nc[proj]
+		temp['nMyC'] = nMyC
+		temp['url'] = url
+		Pf.append(temp)
+	result['projects'] = P
+	#print("done Projects: " + str(len(P)))
+'''	
+	
+	
+
+
 
 
 			
