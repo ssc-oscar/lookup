90cf078813192c3eb9d0f71f7da0b6a5b695c306
dkennard3 <dkennard@vols.utk.edu>
2019-05-27 16:53:59 -0400

    file now on par with dpg.perl, need to add torvald index soon

--- dpg.py
+++ dpg.py
@@ -2,6 +2,7 @@ import pymongo
 import re
 import sys
 from oscar import *
+import subprocess
 
 badProjects = {
   'bb_fusiontestaccount_fuse-2944': '32400A',
@@ -12,9 +13,20 @@ badProjects = {
   'jasperan_github-utils': '4394779C', 
   'avsm_ocaml-ci.logs': '4283368C'}
 
+badAuthors = { 
+  'one-million-repo <mikigal.acc@gmail.com>': "1M commits", 
+  'scraped_page_archive gem 0.5.0 <scraped_page_archive-0.5.0@scrapers.everypolitician.org>': "4243985C", 
+  'Your Name <you@example.com>': "1829654C",
+  'Auto Pilot <noreply@localhost>': "2063212C",
+  'GitHub Merge Button <merge-button@github.com>': "109778A",
+  '= <=>': "190490A",
+  'greenkeeper[bot] <greenkeeper[bot]@users.noreply.github.com>': "2067354C", 
+  'Google Code Exporter <GoogleCodeExporter@users.noreply.github.com>': "277+K projects",
+  'datakit <datakit@docker.com>': "4400778 commits"}
+
 doPrj = False
-doFiles = True
-doBlobs = False
+doFiles = False
+doBlobs = True
 
 client = pymongo.MongoClient("mongodb://da1.eecs.utk.edu/")
 
@@ -73,12 +85,13 @@ gA = {}		## global authors
 u2a = {}
 for user in input.keys():
 	p2a = {}
+	i = 0
 	for proj in a2p[user]:
+		i += 1
 		if proj in badProjects.keys() or proj in p2a.keys():
 			continue 
-		a_names = Project(proj).author_names
 		p2a[proj] = []
-		for author in a_names:
+		for author in Project(proj).author_names:
 			if author in str_version[user]:
 				continue
 			p2a[proj].append(author)
@@ -108,14 +121,12 @@ for user in input.keys():
 	a2c[user] = []
 	for author in input[user]:
 		for commit in Author(author.encode('utf-8')).commit_shas:
-			a2c[user].append(commit)	
+			a2c[user].append(commit)
 
 for author in gA.keys():
 	gA2C[author] = []
 	for commit in Author(author).commit_shas:
 		gA2C[author].append(commit)
-
-## ^^^ takes a LONG time, there are 5,174 authors in gA dictionary
 
 '''
 for key, val in a2c.items():
@@ -123,6 +134,7 @@ for key, val in a2c.items():
 	print(":"),
 	print(val)
 '''
+
 #####################
 ## get blobs for a user
 a2b = {}
@@ -131,12 +143,13 @@ for user in input.keys():
 	for commit in a2c[user]:
 		if Commit(commit).blob_shas_rel:
 			for blob in Commit(commit).blob_shas_rel:
-				a2b[user].append(blob) 
+				a2b[user].append(blob)
+	
 '''
 for key, val in a2b.items():
 	print(key),
 	print(":"),
-	print(val)
+	print(len(val))
 '''
 #####################
 ## get files for a user
@@ -175,11 +188,10 @@ for key, val in p2nc.items():
 	print(val)
 '''
 
-prof = db["profile"].find({})
-PS = db["projects"].find({})
-BS = db["blobs"].find({})
-AS = db["friends"].find({})
-
+prof = db["profile"]
+PS = db["projects"]
+BS = db["blobs"]
+AS = db["friends"]
 
 for user in input.keys():
 	result = {}
@@ -232,7 +244,8 @@ for user in input.keys():
 					for au1 in aaa:
 						if au1 in u2a[user][p]:
 							na += 1
-			pr.append({'nc': nc, 'name': p, 'nAuth': na})
+			pr.append({'nc': nc, 'name': p, 
+						'nAuth': na})
 			if len(A) < 30:
 				A.append({'id': author, 'projects': pr})
 			Af.append({'id': author, 'projects': pr})
@@ -292,11 +305,106 @@ for user in input.keys():
 			F[la] += len(cs) + 1
 
 		result['files'] = F
-		print(result['files'])
-		print("\n")
-		print("\n")
 		
 	if doBlobs:
-		pass
+		c2ta = {}
+		c2h = {}
+		
+		for commit in a2c[user]:
+			if commit not in c2ta.keys():
+				c2ta[commit] = ()
+			c2ta[commit] = Commit_info(commit).time_author
+			c2h[commit] = Commit_info(commit).head
+		
+		b2c = {}
+		b2nc = {}
+		b2na = {}
+		b2depth = {}
+		for blob in blobs:
+			b2c[blob] = []
+			#list of commit_shas for this blob is non-empty
+			if Blob(blob).commit_shas:
+				for commit in Blob(blob).commit_shas:
+					b2c[blob].append(commit)
+			#This blob is stored in one of the "large" tch files, and has too many commits
+			#to store in b2c. We'll skip this blob if that's the case.
+			else:
+				continue
+			bl, nc, csb = blob, len(b2c[blob]), b2c[blob]
+			bas = {}
+			for c in csb:
+				if c in c2ta.keys():
+					t, au = c2ta[c][0], c2ta[c][1]
+					if t not in bas.keys():
+						bas[t] = {}
+					bas[t][au] = c
+					if c2h[c]:
+						depth = int(c2h[c][1], 16)
+						if bl not in b2depth.keys() or b2depth[bl] < depth:
+							b2depth[bl] = depth
+			list = sorted(bas.keys())
+			first = list[0]
+			aa = bas[first]
+			first_auth = aa.keys()
+			if first_auth[0] in input[user]:
+				own = True
+			else:
+				own = False
+			if own:
+				b2na[bl] = {}
+				for t in bas.keys():
+					aa = bas[t]
+					for au in aa:
+						if au not in input[user]:
+							co = bas[t][au]
+							b2na[bl][au] = co
+							if c2h[co]:
+								depth = int(c2h[co][1], 16)
+								if bl not in b2depth.keys() or b2depth[bl] < depth:
+									b2depth[bl] = depth
+			if bl in b2na.keys():
+				b2nc[bl] = nc
+		if b2nc and b2na:
+			for b1, nc in sorted(b2nc.items(), key = lambda kv: kv[1], reverse=True):
+				val = {}
+				for au in b2na[b1]:
+					c = b2na[b1][au]
+					val[c] = au
+				if len(B) < 20:
+					B.append({'blob': b1, 'nc': b2nc[b1], 'depth': b2depth[b1], 'users': val})
+				Bf.append({'blob': b1, 'nc': b2nc[b1], 'depth': b2depth[b1], 'users': val})
+			print("done Blobs")
+			result['blobs'] = B
 
-			
+	if doPrj and doFiles and doBlobs:
+		result['stats'] = { 'NFriends': len(authors)+1, 'NCommits': len(commits)+1, 'NBlobs': len(blobs)+1, 
+							'NFiles': len(files)+1, 'NProjects': len(projects)+1}
+	
+	objects = prof.find({'user': str(user)})
+	if objects.count() >= 0:
+		prof.update_one({'user': str(user)}, {"$set": {key: val for key,val in result.items()}})
+	else:
+		result['user'] = str(user)
+		prof.insert_one({key: val for key, val in result.items()})
+	
+	if doPrj and sys.getsizeof(Pf) < 10000000:
+		objects = PS.find({'user': str(user)})
+		if objects.count() > 0:
+			PS.update_one({'user': str(user)}, {"$set": {'projects': Pf}})
+		else:
+			PS.insert_one({'user': str(user), 'projects': Pf})
+
+	if doBlobs and sys.getsizeof(Bf) < 10000000:
+		objects = BS.find({'user': str(user)})
+		if objects.count() > 0:
+			BS.update_one({'user': str(user)}, {"$set": {'blobs': Bf}})
+		else:
+			BS.insert_one({'user': str(user), 'blobs': Bf})
+
+	if doPrj and sys.getsizeof(Af) < 10000000:
+		objects = AS.find({'user': str(user)})
+		if objects.count() > 0:
+			AS.update_one({'user': str(user)}, {"$set": {'friends': Af}})
+		else:
+			AS.insert_one({'user': str(user), 'friends': Af})
+		
