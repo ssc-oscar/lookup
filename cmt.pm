96656a24fcff8a63692c9a74b142ea33129b58a1
Audris Mockus <audris@utk.edu>
2020-10-12 13:51:41 -0400

    made optimizations for speed

--- cmt.pm
+++ cmt.pm
@@ -16,7 +16,7 @@ use vars qw(@ISA);
 # basic utilities
 
 # obsolete
-my %toUrlMap = ("bb" => "bitbucket.org","gl" => "gitlab.org",
+my %toUrlMap = ("bb" => "bitbucket.org", "gl" => "gitlab.org",
 		"android.googlesource.com" => "android.googlesource.com",
 		"bioconductor.org" => "bioconductor.org",
 		"drupal.com" => "git.drupal.org", "git.eclipse.org" => "git.eclipse.org",
@@ -28,7 +28,9 @@ my %toUrlMap = ("bb" => "bitbucket.org","gl" => "gitlab.org",
 		"kde.org" => "anongit.kde.org",
 		"repo.or.cz" => "repo.or.cz",
 		"salsa.debian.org" => "salsa.debian.org",
-		"sourceforge.net" => "git.code.sf.net/p");
+		"sourceforge.net" => "git.code.sf.net/p"
+    # "git.kernel.org" => "git.kernel.org/pub/scm/",
+    );
 
 sub toUrl {
   my $in = $_[0];
@@ -44,7 +46,6 @@ sub toUrl {
   $in =~ s|_|/|;
   return "https://$in";
 }
-
 
 sub segB {
   my ($s, $n) = @_;
@@ -554,34 +555,6 @@ our %badAuthors = ( 'one-million-repo <mikigal.acc@gmail.com>' => "1M commits",
    'tip-bot for Linus Torvalds <torvalds@linux-foundation.org>' => "",
    'root <root@ubuntu.(none)>' => '');
 
-my %toUrlMap = ("bb" => "bitbucket.org","gl" => "gitlab.org",
-"android.googlesource.com" => "android.googlesource.com",
-"bioconductor.org" => "bioconductor.org",
-"drupal.com" => "git.drupal.org", "git.eclipse.org" => "git.eclipse.org",
-"git.kernel.org" => "git.kernel.org/pub/scm/",
-"git.postgresql.org" => "git.postgresql.org" ,
-"git.savannah.gnu.org" => "git.savannah.gnu.org",
-"git.zx2c4.com" => "git.zx2c4.com" ,
-"gitlab.gnome.org" => "gitlab.gnome.org",
-"kde.org" => "anongit.kde.org",
-"repo.or.cz" => "repo.or.cz",
-"salsa.debian.org" => "salsa.debian.org", 
-"sourceforge.net" => "git.code.sf.net/p");
-
-sub toUrl {
-  my $in = $_[0];
-  my $found = 0;
-  for my $p (keys %toUrlMap){
-    if ($in =~ /^${p}_/ && (scalar(split(/_/, $in)) > 2 || $p eq "sourceforge.net")){
-      $in =~ s|^${p}_|$toUrlMap{$p}\/|;
-      $found ++;
-      last;
-    }
-  }  
-  $in =~ s|^|github.com/| if (!$found);
-  $in =~ s|_|/|;
-  return "https://$in";
-}
 
 
 our %badCmt = (
