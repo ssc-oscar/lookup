446c41c4965e57e9a80cc7b4e2bdcac89737ad69
Audris Mockus <audris@utk.edu>
2021-06-08 15:05:57 -0400

    updating

--- mostWidelyUsed.sh
+++ mostWidelyUsed.sh
@@ -2,6 +2,6 @@
 
 i=$1
 i=$(echo $i |~/lookup/getValues p2P| cut -d\; -f2)
-echo $i |~/lookup/getValues -f P2b | cut -d\; -f2 |/home/audris/bin/lsort 10G > $i.fb
-cat $i.fb | ~/lookup/getValues b2ManyP | /home/audris/bin/lsort 10G -t\; -k2 -rn > $i.fb2n
-head $i.fb2n
+echo $i |~/lookup/getValues -f P2b | cut -d\; -f2 |/home/audris/bin/lsort 10G > $i.b
+cat $i.b | ~/lookup/getValues b2ManyP | /home/audris/bin/lsort 10G -t\; -k2 -rn > $i.b2n
+head $i.b2n
