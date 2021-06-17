c1889eb9c1b51ce1ea99a62e934055bfaaa975cf
Audris Mockus <audris@utk.edu>
2021-06-17 09:45:22 -0400

    finalizing comparison of projects in terms of shared blobs

--- cmpO.sh
+++ cmpO.sh
@@ -13,13 +13,13 @@ echo $j |~/lookup/getValues -f P2fb | cut -d\; -f2 |sort > $j.fb
 
 #output summary
 echo comparing $i and $j 
-echo $(join $i.fb $j|wc -l) created in $i used in $j
-echo $(join $i $j.fb|wc -l) created in $j used in $i
-echo $(join $i $j|wc -l) shared 
-echo $(join -v1 $i $j|wc -l) uniq to $i
-echo $(join -v2 $i $j|wc -l) unique to $j
+echo $(join $i.fb $j|wc -l) blobs created in $i used in $j
+echo $(join $i $j.fb|wc -l) blobs created in $j used in $i
+echo $(join $i $j|wc -l) shared  between $i and $j
+echo $(join -v1 $i $j|wc -l) blobs unique to $i
+echo $(join -v2 $i $j|wc -l) blobs unique to $j
 #list shared blobs
-echo "creted in $i and present in $j
+echo created in $i and present in $j
 join $i.fb $j|~/lookup/getValues  b2f|cut -d\; -f1,2
-echo "creted in $j and present in $i
+echo created in $j and present in $i
 join $j.fb $i|~/lookup/getValues  b2f|cut -d\; -f1,2
